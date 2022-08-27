07f87d5995cb2c139465a1b0cfd1b4a1a7c0990d
Tomas-Man <tmgitxxi@outlook.com>
2022-08-27 07:11:29 +0200

    5.2

--- .svelte-kit/generated/client-manifest.js
+++ .svelte-kit/generated/client-manifest.js
@@ -21,14 +21,14 @@ export const components = [
 export const dictionary = {
 	"": [[0, 13], [1]],
 	"privacidad": [[0, 14], [1]],
-	"components/CarruselNoticias": [[0, 2], [1]],
+	"components/carruselnoticias": [[0, 2], [1]],
 	"components/background": [[0, 3], [1]],
 	"components/cabecera": [[0, 4], [1]],
 	"components/carousel": [[0, 5], [1]],
 	"components/footer": [[0, 6], [1]],
 	"components/formulario": [[0, 7], [1]],
-	"components/noticiaFoto": [[0, 8], [1]],
-	"components/noticiaVideo": [[0, 9], [1]],
+	"components/noticiafoto": [[0, 8], [1]],
+	"components/noticiavideo": [[0, 9], [1]],
 	"estudios/carroceria": [[0, 10], [1]],
 	"estudios/eso": [[0, 11], [1]],
 	"estudios/mantenimiento": [[0, 12], [1]]
