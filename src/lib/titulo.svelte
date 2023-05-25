cbcbc3859201b328fab84d7b126817368f15d51e
Tomas-Man <tmgitxxi@outlook.com>
2023-05-25 23:43:57 +0200

    11

---
+++ src/lib/titulo.svelte
@@ -0,0 +1,26 @@
+<section class="titulo">
+    <h1></h1>
+    <h2></h2>
+</section>
+<style>
+    .titulo{
+        position: relative;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 50vh;
+    }
+    h1{
+        font-size: 3em;
+        color: white;
+        display: grid;
+        place-items: center;
+    }
+    h2{
+        font-size: 3em;
+        color: white;
+        display: grid;
+        place-items: center;
+        font-weight: lighter;
+    }
+</style>
