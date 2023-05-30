383ba856becefc8c7aff9207be048a6f6e620e43
Tomas-Man <tmgitxxi@outlook.com>
2023-05-30 12:20:36 +0200

    13.6

---
+++ src/lib/escuela.svelte
@@ -0,0 +1,80 @@
+
+
+    <section class="contenidoEscuela">
+        <slot name="slotContenidoIzq">
+            <div class="contIzq">
+                <h2></h2>
+                <br>
+                <p></p>
+            </div>
+        </slot>
+        <slot name="slotContenidoDcha">
+            <div class="contDcha">
+                <div class="contImg">
+                    <figure></figure>
+                </div>
+            </div>
+        </slot>
+    </section>
+
+<style>
+    @font-face {
+        font-family: "Decima Nova Pro";
+        src: url("/fonts/DecimaNovaPro.otf");
+    }
+    *{
+        font-family: "Decima Nova Pro", sans-serif;
+    }/*
+    .fondo{
+        position: fixed;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100vh;
+        background-image: url("/img/escuela/erasmus/europa.jpg");
+        background-size: cover;
+        background-position: center;
+        background-repeat: no-repeat;
+        z-index: -1;
+    }*/
+    .contenidoEscuela{
+        position: relative;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        background-color: #d9d9d9;
+        display: flex;
+    }
+    .contIzq{
+        position: relative;
+        top: 0;
+        left: 0;
+        width: calc(50% - 200px);
+        height: 100%;
+        margin: 100px 100px 120px;
+    }
+    .contIzq h2{
+        font-size: 3em;
+        font-weight: lighter;
+        text-align: center;
+    }
+    .contIzq p{
+        font-size: 1.5em;
+        margin-top: 20px;
+        color: green;
+    }
+    .contDcha{
+        position: relative;
+        top: 0;
+        left: 0;
+        margin: 40px 40px 40px 0;
+    }
+    .contImg{
+        position: relative;
+        top: 0;
+    }/*
+    .contImg img{
+        width: calc(50vw - 80px);
+    }*/
+</style>
--- src/routes/escuela/erasmus/index.svelte
+++ src/routes/escuela/erasmus/index.svelte
@@ -1,23 +1,25 @@
 <script>
-    import '../../../styles/escuela.css';
     import Titulo from '../../../lib/titulo.svelte';
+    import Escuela from '../../../lib/escuela.svelte';
 </script>
-<main>
-    <slot name="slotFondo">
-        <div class="fondo"></div>
-    </slot>
-    <Titulo>
-        <h1>Servicio de Erasmus +</h1>
-        <h2>Fomento de la educación Europea</h2>
-    </Titulo>
-    <!--<section class="tituloEscuela">
-        <slot name="slotTituloEsc">
-            <div class="tituloEsc">
-                <h1>Servicio Erasmus+</h1>
-                <h2>Fomento de la educación en Europa</h2>
-            </div>
-        </slot>
-    </section> -->
+<div class="fondo"></div>
+<Titulo>
+    <h1>Servicio de Erasmus +</h1>
+    <h2>Fomento de la educación Europea</h2>
+</Titulo>
+<Escuela>
+    <div class="contIzq" slot="slotContenidoIzq">
+        <h2>¿Qué es Erasmus+?</h2>
+        <br>
+        <p>El <a href="http://sepie.es/">Servicio Español para la Internacionalización de la Educación</a> nos concede para este curso el programa<a href="https://erasmus-plus.ec.europa.eu/es">ErasmusPlus</a>, el programa de la Unión Europea para apoyar la educación, la formación, la juventud y el deporte en Europa.</p>
+    </div>
+    <div class="contDcha" slot="slotContDcha">
+        <div class="contImg">
+            <img src="/img/escuela/erasmus/erasmus_SEPIe.jpeg" alt="Erasmus">
+        </div>
+    </div>
+</Escuela>
+<!--
     <section class="contenidoEscuela">
         <slot name="slotContenidoIzq">
             <div class="contIzq">
@@ -35,10 +37,17 @@
                 </div>
             </div>
         </slot>
-    </section>
-</main>
+    </section>-->
+
 <style>
-    .fondo {
+    @font-face {
+        font-family: "Decima Nova Pro";
+        src: url("/fonts/DecimaNovaPro.otf");
+    }
+    *{
+        font-family: "Decima Nova Pro", sans-serif;
+    }
+    .fondo{
         position: fixed;
         top: 0;
         left: 0;
@@ -49,5 +58,42 @@
         background-position: center;
         background-repeat: no-repeat;
         z-index: -1;
+    }/*
+    .contIzq{
+        position: relative;
+        top: 0;
+        left: 0;
+        width: calc(50% - 200px);
+        height: 100%;
+        margin: 100px 100px 120px;
+    }
+    .contIzq h2{
+        font-size: 3em;
+        font-weight: lighter;
+        text-align: center;
+    }
+    .contIzq p{
+        font-size: 1.5em;
+        margin-top: 20px;
+    }*/
+    .contIzq a{
+        color: #4a8089;
+    }
+    .contIzq a:hover{
+        text-decoration: none;
+        color: #75b3b7;
+    }/*
+    .contDcha{
+        position: relative;
+        top: 0;
+        left: 0;
+        margin: 40px 40px 40px 0;
+    }
+    .contImg{
+        position: relative;
+        top: 0;
+    }*/
+    .contImg img{
+        width: calc(50vw - 80px);
     }
 </style>
