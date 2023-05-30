e2afc6eeb56364b971ac4d98d92696fee7d6cc8e
Tomas-Man <tmgitxxi@outlook.com>
2023-05-30 12:33:05 +0200

    13.6

--- src/routes/escuela/erasmus/erasmus.svelte
+++ src/routes/escuela/erasmus/erasmus.svelte
@@ -1,6 +1,25 @@
 <script>
-    import Escuela from './index.svelte';
+    import Escuela from '../../../lib/escuela.svelte';
+    import Titulo from '../../../lib/titulo.svelte';
 </script>
+<div class="fondo"></div>
+<Titulo>
+    <h1>Servicio Erasmus+</h1>
+    <h2>Fomento de la educación en Europa</h2>
+</Titulo>
+<Escuela>
+    <div class="contIzq" slot="slotContenidoIzq">
+        <h2>¿Qué es Erasmus+?</h2>
+        <br>
+        <p>El <a href="http://sepie.es/">Servicio Español para la Internacionalización de la Educación</a> nos concede para este curso el programa<a href="https://erasmus-plus.ec.europa.eu/es">ErasmusPlus</a>, el programa de la Unión Europea para apoyar la educación, la formación, la juventud y el deporte en Europa.</p>
+    </div>
+    <div class="conDcha" slot="slotContDcha">
+        <div class="excelenciaImg">
+            <img src="/img/escuela/excelencia/FP_excelencia.jpeg" alt="Excelencia">
+        </div>
+    </div>
+</Escuela>
+<!--
 <Escuela>
     <div slot="slotFondo" class="fondo"></div>
     <div slot="slotTituloEsc" class="tituloEsc">
@@ -20,3 +39,25 @@
         </div>
     </div>
 </Escuela>
+-->
+<style>
+    .fondo{
+        position: fixed;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100vh;
+        background-image: url("/img/escuela/erasmus/europa.jpg");
+        background-size: cover;
+        background-position: center;
+        background-repeat: no-repeat;
+        z-index: -1;
+    }
+    .contIzq a{
+        color: #4a8089;
+    }
+    .contIzq a:hover{
+        text-decoration: none;
+        color: #75b3b7;
+    }
+</style>
