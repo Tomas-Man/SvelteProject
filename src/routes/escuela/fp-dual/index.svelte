9d4e883bf218328d168023fcb5603470e9710d87
Tomas-Man <tmgitxxi@outlook.com>
2023-05-31 05:00:50 +0200

    13.7

--- src/routes/escuela/fp-dual/index.svelte
+++ src/routes/escuela/fp-dual/index.svelte
@@ -104,13 +104,6 @@
         font-size: 1.4em;
     }
 
-    h2 {
-        font-size: 3em;
-        font-weight: lighter;
-        text-align: center;
-        margin-bottom: 20px;
-    }
-
     .logoFP {
         position: relative;
         width: 50%;
